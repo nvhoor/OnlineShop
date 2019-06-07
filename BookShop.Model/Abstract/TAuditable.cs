f52f40917a81b58e0163a21da0a806aa9d68ae16
Administrator <Administrator@IK-PC>
2019-06-07 10:43:54 +0700

    move file

---
+++ BookShop.Model/Abstract/TAuditable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Model.Models
+{
+    class TAuditable : IAuditable
+    {
+        [MaxLength(50)]
+        public DateTime CreatedBy
+        {
+            get;
+
+            set;
+        }
+
+        public DateTime CreatedDate
+        {
+            get;
+
+
+            set;
+
+        }
+        [MaxLength(250)]
+        public string MetalDescription
+        {
+            get
+          ;
+
+            set
+           ;
+        }
+        [MaxLength(250)]
+        public string MetalKeyword
+        {
+            get
+           ;
+
+            set
+           ;
+        }
+        [Required]
+        public bool Status
+        {
+            get
+            ;
+            set
+           ;
+        }
+        [MaxLength(50)]
+        public DateTime? UpdatedBy
+        {
+            get
+           ;
+
+            set
+           ;
+        }
+
+        public DateTime? UpdatedDate
+        {
+            get
+           ;
+
+            set
+            ;
+        }
+    }
+}
